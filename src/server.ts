7539384877522d02facc96fec8ab8c9fc4853f95
Your Personal Name <gauravyadav2099@gmail.com>
2024-06-30 22:10:45 +0530

    frontend serve

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { chatRouter } from "./routes/chat";
 import { initializeChain } from "./initChain";
+import path from "path";
 
 dotenv.config();
 
@@ -12,8 +13,10 @@ app.use(express.json());
 
 const PORT = 4004;
 initializeChain();
-app.use("/chat", chatRouter);
-
+app.use("/api/chat", chatRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
